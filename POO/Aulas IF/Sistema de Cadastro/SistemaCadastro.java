7fa705d1335d0cde5354d0f1669dde400973f2d8
Eduardo Rigon <70084379+edukatiau@users.noreply.github.com>
2023-10-12 12:57:07 -0300

    criando user

--- POO/Aulas IF/Sistema de Cadastro/SistemaCadastro.java
+++ POO/Aulas IF/Sistema de Cadastro/SistemaCadastro.java
@@ -1,5 +1,13 @@
 public class SistemaCadastro{
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        Usuario user = new Usuario();
+
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        String login = sc.next();
+        String senha = sc.next();
+
+       
     }
 }
