74fea8a02644fba28eef4198ea6e14e03da569c3
Eduardo Rigon <edukatiau@gmail.com>
2023-10-13 20:17:20 -0300

    menu, cadastro e login

--- POO/Aulas IF/Sistema de Cadastro/SistemaCadastro.java
+++ POO/Aulas IF/Sistema de Cadastro/SistemaCadastro.java
@@ -1,14 +1,65 @@
-public class SistemaCadastro{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+//import java.util.Scanner;
 
-        String name = sc.nextLine();
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SistemaCadastro {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ArrayList<Usuario> users = new ArrayList<Usuario>();
+        menu(users);
+    }
+
+    public static void menu(ArrayList<Usuario> u) {
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Login");
+        System.out.println("2 - Cadastro");
+        System.out.println("0 - Sair");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                login(u);
+
+            case 2:
+                cadastro(u);
+
+            case 0:
+                break;
+        }
+    }
+
+    public static void cadastro(ArrayList<Usuario> u) {
+        System.out.println("Qual seu nome? ");
+        String name = sc.next();
+        System.out.println("Qual sua idade? ");
         int age = sc.nextInt();
+        System.out.println("Qual seu login? ");
         String login = sc.next();
-        String senha = sc.next();
+        System.out.println("Qual sua senha? ");
+        String pass = sc.next();
 
-       Usuario user = new Usuario(name, age, login, senha);
+        u.add(new Usuario(name, age, login, pass));
+        System.out.println("Usuário cadastrado com sucesso!");
+        menu(u);
+    }
 
-       System.out.println(user);
+    public static void login(ArrayList<Usuario> u) {
+
+        System.out.println("Insira seu login:");
+        String log = sc.next();
+        System.out.println("Insira sua senha:");
+        String pass = sc.next();
+
+        for (Usuario user : u) {
+            if (user.getLogin().equals(log) && user.getSenha().equals(pass)) {
+                System.out.println("Seja bem vindo(a), " + user.getNome() + "!");
+            } else{
+                System.out.println("Login ou senha inválidos");
+                break;
+            }
+        }
+        menu(u);
     }
 }
