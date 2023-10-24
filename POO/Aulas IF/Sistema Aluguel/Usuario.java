09af119be97e300b71f282a92c196a77443869b0
Eduardo Rigon <70084379+edukatiau@users.noreply.github.com>
2023-10-24 12:58:12 -0300

    usuario

--- POO/Aulas IF/Sistema Aluguel/Usuario.java
+++ POO/Aulas IF/Sistema Aluguel/Usuario.java
@@ -1,7 +1,42 @@
 public class Usuario{
 
-    public Usuario(){
-        
+    private String nome;
+    private String login;
+    private String senha;
+
+    public Usuario(String nome, String login, String senha){
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String newNome){
+        this.nome = newNome;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public void setLogin(String newLogin){
+        this.login = newLogin;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+
+    public void setSenha(String newSenha){
+        this.senha = newSenha;
+    }
+
+    @Override
+    public String toString(){
+        return "Usuario [name= " +nome+ ", login= " +login+ ", senha= " +senha+ "]";
     }
 
 }
