2fa771d0c2723a4f6704943b238499e2365753d5
Eduardo Rigon <70084379+edukatiau@users.noreply.github.com>
2023-10-24 13:04:35 -0300

    sys

--- POO/Aulas IF/Sistema Aluguel/SistemaAluguel.java
+++ POO/Aulas IF/Sistema Aluguel/SistemaAluguel.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class SistemaAluguel{
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
-        
+        List<Usuario> usuarios = new ArrayList<Usuario>;
+
+        usuarios.add(new Usuario("edu", "edu", "123"));
+    }
+
+    public static login(ArrayList<Usuario> usuarios){
+        String log = sc.next();
+        String pass = sc.next();
+
+        for(Usuario u: usuarios){
+            if(log.equals(u.getLogin()) && pass.equals(u.getSenha())){
+                System.out.println("Logado")
+            }
+        }
     }
 }
